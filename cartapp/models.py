c7c339003eb23bc45b3f21dd2a556da74f5dccf8
rodgers-ctrl <rodgerskiria25@gmail.com>
2026-02-23 13:33:52 +0300

    updated4

--- cartapp/models.py
+++ cartapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    productname = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.productname
